74036e851a03d204cbcc85bb516a713410adaf6b
Collin Barber <collinjbarber@gmail.com>
2025-06-03 17:23:45 -0400

    fix relocations

--- mechanicscore-build/build.gradle.kts
+++ mechanicscore-build/build.gradle.kts
@@ -50,45 +50,15 @@ tasks.shadowJar {
     val versionProperty = findProperty("mechanicscore.version") as? String ?: throw IllegalArgumentException("mechanicscore.version was null")
     archiveFileName.set("MechanicsCore-$versionProperty.jar")
 
-    dependencies {
-        // Main project code
-        include(project(":mechanicscore-core"))
+    val libPackage = "me.deecaad.core.lib"
 
-        // Platforms and hook modules
-        file("../mechanicscore-hooks").listFiles()?.filter { it.isDirectory }?.forEach {
-            include(project(":${it.name}"))
-        }
-        file("../mechanicscore-platforms/paper").listFiles()?.filter { it.isDirectory }?.forEach {
-            include(project(":${it.name}"))
-        }
-
-        /**
-         * Shades the library into a new location, to avoid conflicts with other
-         * plugins that may use the same library.
-         */
-        fun relocateLib(
-            fromPackage: String,
-            toPackage: String,
-            lib: Provider<MinimalExternalModuleDependency>? = null,
-        ) {
-            relocate(fromPackage, toPackage) {
-                if (lib != null) {
-                    val group = lib.get().group
-                    val name = lib.get().name
-                    include(dependency("$group:$name"))
-                }
-            }
-        }
-
-        val libPackage = "me.deecaad.core.lib"
-        relocateLib("net.kyori", "$libPackage.kyori", libs.adventureApi)
-        relocateLib("net.kyori", "$libPackage.kyori", libs.adventureBukkit)
-        relocateLib("net.kyori", "$libPackage.kyori", libs.adventureTextLegacy)
-        relocateLib("net.kyori", "$libPackage.kyori", libs.adventureTextMinimessage)
-        relocateLib("dev.jorel.commandapi", "$libPackage.commandapi", libs.commandApiShade)
-        relocateLib("com.cjcrafter.foliascheduler", "$libPackage.scheduler", libs.foliaScheduler)
-        relocateLib("com.zaxxer.hikari", "$libPackage.hikari", libs.hikariCp)
-        relocateLib("kotlin.", "$libPackage.kotlin.", libs.kotlinStdlib)
-        relocateLib("com.cryptomorin.xseries", "$libPackage.xseries", libs.xSeries)
-    }
+    relocate("org.slf4j", "$libPackage.slf4j")
+    relocate("org.bstats", "$libPackage.bstats")
+    relocate("net.kyori", "$libPackage.kyori")
+    relocate("com.jeff_media.updatechecker", "$libPackage.updatechecker")
+    relocate("dev.jorel.commandapi", "$libPackage.commandapi")
+    relocate("com.cjcrafter.foliascheduler", "$libPackage.scheduler")
+    relocate("com.zaxxer.hikari", "$libPackage.hikari")
+    relocate("kotlin.", "$libPackage.kotlin.")
+    relocate("com.cryptomorin.xseries", "$libPackage.xseries")
 }
